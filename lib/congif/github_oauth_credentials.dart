e248018a71a208fe133ff04b29b66c7dfdb062f4
sasnigdho <salmanahmedims@gmail.com>
2023-03-16 22:36:07 +0600

    Github credentials file added.

---
+++ lib/congif/github_oauth_credentials.dart
@@ -0,0 +1,5 @@
+const githubClientId = '00a7cc6e5873d3efe80c';
+const githubClientSecret = 'b6ae1da624a01584acfe73f9dc4e5f7c280203aa';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
